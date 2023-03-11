4c83a0d2bca988bd26db46f2c8c19ac533bc2f20
Dicola <dicolber@amazon.com>
2023-03-11 06:41:06 -0500

    not sure

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+
+
+
+
+function writeCards(names, event) {
+    let eventMessage = []
+    for (let i = 0; i < names.length; i++) {
+        eventMessage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return eventMessage
+
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown() {
+    let i = 0;
+    while (i < 10) {
+ }
+ console.log(countDown(10));
+
+}
+
